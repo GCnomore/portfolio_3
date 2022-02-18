3b1c8d699c8a6f0d84eb1104f879ccf2408b5bd7
isaac <isaacgc08@gmail.com>
2022-02-17 22:20:59 -0800

    background cover done

---
+++ src/components/Background/Background.tsx
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import _ from "lodash";
+
+import "./animation.css";
+import * as BG from "./Background_Styled";
+
+export default function Background() {
+  const sw: number = window.screen.availWidth;
+  const sh: number = window.screen.availHeight;
+  const rows: number = sw / 30 - 2;
+  const total: number = Math.round(sh / rows) * 30 + 60;
+
+  const renderTiles = (): JSX.Element[] => {
+    const tiles: JSX.Element[] = [];
+    for (let i: number = 0; i < total; i++) {
+      tiles.push(
+        <BG.Tiles
+          key={`tile${i}`}
+          className={`tile-${i} tiles`}
+          onClick={() => i == 30 && addAnimation()}
+          size={rows}
+          delay={i}
+        />
+      );
+    }
+
+    return tiles;
+  };
+
+  const addAnimation = (): void => {
+    const tiles = document.querySelectorAll(".tiles");
+
+    //* Randomly take off tiles
+    _.shuffle(tiles).forEach((item, index) => {
+      setTimeout(() => {
+        item.classList.add("fallOff");
+      }, index * 6);
+    });
+
+    //* Fadeout when tiles are almost gone
+    setTimeout(() => {
+      document.querySelector(".bgContainer")?.classList.add("fadeOut");
+    }, (total - 1) * 6 - 100);
+
+    //! Too may DOM elements... Remove
+    setTimeout(() => {
+      document.querySelector(".bgContainer")?.remove();
+    }, (total - 1) * 6 + 1600);
+  };
+
+  return (
+    <BG.Container className="bgContainer" bgColor={"#121212"}>
+      {renderTiles()}
+    </BG.Container>
+  );
+}
---
+++ src/components/Background/Background_Styled.ts
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const Container = styled.div<{ bgColor: string }>`
+  display: flex;
+  justify-content: center;
+  width: 100vw;
+  height: 100vh;
+  flex-wrap: wrap;
+  overflow: hidden;
+  background-color: ${({ bgColor }) => bgColor};
+`;
+
+export const Tiles = styled.div<{
+  size: number;
+  delay: number;
+}>`
+  width: ${({ size }) => size - 4}px;
+  height: ${({ size }) => size - 4}px;
+  border: 1px #121212 solid;
+  background-color: #191919;
+`;
