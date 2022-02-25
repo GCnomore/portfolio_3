ab1509c739b854db8ec2ac3a0ca7abc15a351c06
isaac <isaacgc08@gmail.com>
2022-02-24 21:53:53 -0800

    lightning animation

--- src/sections/Home/Home.tsx
+++ src/sections/Home/Home.tsx
@@ -1,21 +1,51 @@
 import React, { useState } from "react";
+import * as Styled from "./Home_Styled";
 import Background from "../../components/Background/Background";
 import About from "../About/About";
 import Cover from "../Cover/Cover";
 import Projects from "../Projects/Projects";
-import * as Styled from "./Home_Styled";
+import Lightning1 from "../../images/lightning1.webp";
+import Lightning2 from "../../images/lightning2.webp";
 
 export default function Home() {
   const [isBgGone, setIsBgGone] = useState<boolean>(false);
+  const [strike, setStrike] = useState<{ show: boolean; count: number }>({
+    show: false,
+    count: 0,
+  });
+
+  const trackScroll = (e: React.UIEvent<HTMLDivElement>): void => {
+    const scrollHeight: number = e.currentTarget.scrollHeight;
+    const currentHeight: number = e.currentTarget.scrollTop;
+
+    if (
+      currentHeight >= scrollHeight * 0.3 &&
+      currentHeight <= scrollHeight * 0.32 &&
+      strike.count === 0
+    ) {
+      setStrike({ show: true, count: 0 });
+      setTimeout(() => {
+        setStrike({ show: false, count: 1 });
+      }, 1000);
+      console.log("here");
+    }
+  };
 
   return (
-    <Styled.Container>
+    <Styled.Container onScroll={trackScroll}>
       {/* Background will be removed from DOM once animation is over */}
       <Background setIsBgGone={setIsBgGone} />
 
       <Cover isBgGone={isBgGone} />
       <About isBgGone={isBgGone} />
       <Projects isBgGone={isBgGone} />
+      {strike.show && (
+        <>
+          <Styled.LightningCover strike={strike.show} />{" "}
+          <Styled.Lightning1 strike={strike.show} src={Lightning1} />
+          <Styled.Lightning2 strike={strike.show} src={Lightning2} />
+        </>
+      )}
     </Styled.Container>
   );
 }
--- src/sections/Home/Home_Styled.ts
+++ src/sections/Home/Home_Styled.ts
@@ -1,4 +1,37 @@
-import styled from "styled-components/macro";
+import styled, { keyframes } from "styled-components/macro";
+
+const Strike = keyframes`
+  0%{
+    opacity: 1;
+    display: flex;
+  }
+  60%{
+    opacity: 1;
+    display: flex;
+  }
+  80%{
+  opacity: 0;
+  }
+  100%{
+  opacity: 0;
+  display: none;
+  }
+`;
+
+const Lightning = keyframes`
+  0%{
+    opacity: 1;
+    display: flex;
+  }
+  30%{
+    opacity: 0;
+    display: none;
+  }
+  100%{
+    opacity: 0;
+    display: none;
+  }
+`;
 
 export const Container = styled.main`
   display: flex;
@@ -9,4 +42,38 @@ export const Container = styled.main`
   perspective: 10px;
   overflow-y: auto;
   overflow-x: hidden;
+`;
+
+export const LightningCover = styled.div<{ strike: boolean }>`
+  position: fixed;
+  height: 600vh;
+  width: 100vw;
+  background: radial-gradient(transparent 50%, #242424), white;
+  top: 0;
+  animation-name: ${({ strike }) => strike && Strike};
+  animation-duration: 1s;
+  animation-fill-mode: forwards;
+  z-index: ${({ strike }) => (strike ? 100 : -1)};
+`;
+
+export const Lightning1 = styled.img<{ strike: boolean }>`
+  position: absolute;
+  height: 100vh;
+  top: 120vh;
+  left: 10vw;
+  animation-name: ${({ strike }) => strike && Lightning};
+  animation-duration: 2s;
+  animation-fill-mode: forwards;
+  z-index: ${({ strike }) => (strike ? 100 : -1)};
+`;
+
+export const Lightning2 = styled.img<{ strike: boolean }>`
+  position: absolute;
+  width: 30vw;
+  top: 130vh;
+  right: 0px;
+  animation-name: ${({ strike }) => strike && Lightning};
+  animation-duration: 2s;
+  animation-fill-mode: forwards;
+  z-index: ${({ strike }) => (strike ? 100 : -1)};
 `;
